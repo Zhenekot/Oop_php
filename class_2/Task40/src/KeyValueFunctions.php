20ba30c00d84b04ade9d7387e0f7b88ecc1474f3
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-11-20 19:09:53 +0500

    Add files via upload

--- class_2/Task40/src/KeyValueFunctions.php
+++ class_2/Task40/src/KeyValueFunctions.php
@@ -2,19 +2,17 @@
 
 namespace App\KeyValueFunctions;
 use App\InMemoryKV;
+use Illuminate\Support\Collection;
 function swapKeyValue(InMemoryKV $data):InMemoryKV{
-    $newData =[];
-    foreach ($data->toArray() as $key => $value){
-        $newData[$value] = $key;
-    }
-   
+
+    $collection = collect($data->toArray())->flip();
+
     foreach ($data->toArray() as $key => $value){
         $data->unset($key);
     }
 
-    foreach ($newData as $key => $value){
+    foreach ($collection as $key => $value){
         $data->set($key, $value);
     }
-
     return $data;
 }
