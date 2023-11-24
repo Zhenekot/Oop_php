ebec684c79b9483cc2cd3e405ae6e7bd30c9c5c0
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-11-24 14:17:02 +0500

    Add files via upload

--- class_2/Task46/src/parsers/JsonParser.php
+++ class_2/Task46/src/parsers/JsonParser.php
@@ -3,14 +3,8 @@
 namespace App\parsers;
 
 class JsonParser{
-    private string $path;
-
-    public function __construct(string $path) {
-        $this->path = $path;
-    }
-    public function parse():array{
-        $data = file_get_contents($this->path);
-        $data = json_decode($data, true);
-        return $data;
+    
+    public function parse($data):array{
+        return json_decode(file_get_contents($data), true);
     }
 }
--- class_2/Task46/src/parsers/YamlParser.php
+++ class_2/Task46/src/parsers/YamlParser.php
@@ -5,14 +5,7 @@ namespace App\parsers;
 use Symfony\Component\Yaml\Yaml;
 
 class YamlParser{
-    private string $path;
-
-    public function __construct(string $path) {
-        $this->path = $path;
-    }
-    public function parse():array{
-        $data = file_get_contents($this->path);
-        $data = Yaml::parse($data, true);
-        return $data;
+    public function parse($data):array{
+        return Yaml::parse(file_get_contents($data), true);;
     }
 }
